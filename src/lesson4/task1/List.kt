c6d6a574303df5abded4405d02be27c07aaeb918
AndreySolodukh <43181564+AndreySolodukh@users.noreply.github.com>
2018-10-15 16:49:38 +0300

    Решено большинство задач.

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -115,14 +115,14 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double = sqrt(v.map(fun(it: Double) = it * it).sum())
 
 /**
  * Простая
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double = if (list.isEmpty()) 0.0 else list.sum() / list.size
 
 /**
  * Средняя
@@ -132,7 +132,11 @@ fun mean(list: List<Double>): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> {
+    val mean = mean(list)
+    if (list.isNotEmpty()) for (i in 0 until list.size) list[i] -= mean
+    return list
+}
 
 /**
  * Средняя
@@ -141,7 +145,11 @@ fun center(list: MutableList<Double>): MutableList<Double> = TODO()
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.0.
  */
-fun times(a: List<Double>, b: List<Double>): Double = TODO()
+fun times(a: List<Double>, b: List<Double>): Double {
+    var answer = 0.0
+    for (i in 0 until a.size) answer += a[i] * b[i]
+    return answer
+}
 
 /**
  * Средняя
@@ -151,7 +159,15 @@ fun times(a: List<Double>, b: List<Double>): Double = TODO()
  * Коэффициенты многочлена заданы списком p: (p0, p1, p2, p3, ..., pN).
  * Значение пустого многочлена равно 0.0 при любом x.
  */
-fun polynom(p: List<Double>, x: Double): Double = TODO()
+fun polynom(p: List<Double>, x: Double): Double {
+    var multiplier = 1.0
+    var answer = 0.0
+    if (p.isNotEmpty()) for (i in 0 until p.size) {
+        answer += p[i] * multiplier
+        multiplier *= x
+    }
+    return answer
+}
 
 /**
  * Средняя
@@ -163,7 +179,10 @@ fun polynom(p: List<Double>, x: Double): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun accumulate(list: MutableList<Double>): MutableList<Double> = TODO()
+fun accumulate(list: MutableList<Double>): MutableList<Double> {
+    if (list.isNotEmpty()) for (i in 1 until list.size) list[i] += list[i - 1]
+    return list
+}
 
 /**
  * Средняя
@@ -172,7 +191,18 @@ fun accumulate(list: MutableList<Double>): MutableList<Double> = TODO()
  * Результат разложения вернуть в виде списка множителей, например 75 -> (3, 5, 5).
  * Множители в списке должны располагаться по возрастанию.
  */
-fun factorize(n: Int): List<Int> = TODO()
+fun factorize(n: Int): List<Int> {
+    var answer = mutableListOf<Int>()
+    var helper = n
+    var multiplier = 2
+    while (helper > 1) if (helper % multiplier == 0) {
+        answer.add(multiplier)
+        helper /= multiplier
+        multiplier = 2
+    } else multiplier += 1
+    return answer.sorted()
+    // Требует доработки.
+}
 
 /**
  * Сложная
@@ -181,7 +211,7 @@ fun factorize(n: Int): List<Int> = TODO()
  * Результат разложения вернуть в виде строки, например 75 -> 3*5*5
  * Множители в результирующей строке должны располагаться по возрастанию.
  */
-fun factorizeToString(n: Int): String = TODO()
+fun factorizeToString(n: Int): String = factorize(n).joinToString(separator = "*")
 
 /**
  * Средняя
@@ -190,7 +220,20 @@ fun factorizeToString(n: Int): String = TODO()
  * Результат перевода вернуть в виде списка цифр в base-ичной системе от старшей к младшей,
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
-fun convert(n: Int, base: Int): List<Int> = TODO()
+fun convert(n: Int, base: Int): List<Int> {
+    var answer = mutableListOf<Int>()
+    var helper = n
+    while (helper > 0) {
+        answer.add(helper % base)
+        helper /= base
+    }
+    for (i in 0 until answer.size / 2) {
+        helper = answer[answer.size - 1 - i]
+        answer[answer.size - 1 - i] = answer[i]
+        answer[i] = helper
+    }
+    return answer
+}
 
 /**
  * Сложная
@@ -200,7 +243,16 @@ fun convert(n: Int, base: Int): List<Int> = TODO()
  * строчными буквами: 10 -> a, 11 -> b, 12 -> c и так далее.
  * Например: n = 100, base = 4 -> 1210, n = 250, base = 14 -> 13c
  */
-fun convertToString(n: Int, base: Int): String = TODO()
+fun convertToString(n: Int, base: Int): String {
+    val alph = "abcdefghijklmnopqrstuvwxyz"
+    var answer = ""
+    var helper = n
+    while (helper > 0) {
+        answer = if (helper % base < 10) (helper % base).toString() + answer else alph[(helper % base) - 10] + answer
+        helper /= base
+    }
+    return answer
+}
 
 /**
  * Средняя
@@ -239,4 +291,70 @@ fun roman(n: Int): String = TODO()
  * Например, 375 = "триста семьдесят пять",
  * 23964 = "двадцать три тысячи девятьсот шестьдесят четыре"
  */
-fun russian(n: Int): String = TODO()
+fun russian(n: Int): String {
+    val deg12 = listOf("одна", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять",
+            "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать",
+            "восемнадцать", "девятнадцать")
+    val deg2 = listOf("двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят",
+            "девяносто")
+    val deg3 = listOf("сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот",
+            "девятьсот")
+    val opti = listOf("тысяча", "тысячи", "тысяч", "две")
+    var helper = n
+    var answer = ""
+    var count = 0
+    while (helper > 0) {
+        count += 1
+        if ((count == 1) and ((helper % 100) in 1..19)) {
+            answer = deg12[helper % 100]
+            count += 1
+            helper /= 100
+            continue
+        }
+        if (count == 4) {
+            when {
+                (helper % 10 == 1) and (helper % 100 != 11) -> {
+                    answer = deg12[0] + " " + opti[0] + " " + answer
+                    helper /= 10
+                }
+                helper % 100 in 11..19 -> {
+                    answer = deg12[helper % 100] + " " + opti[2] + " " + answer
+                    helper /= 100
+                    count += 1
+                }
+                helper % 10 == 2 -> {
+                    answer = opti[3] + " " + opti[1] + " " + answer
+                    helper /= 10
+                }
+                helper % 10 in 3..4 -> {
+                    answer = deg12[helper % 10] + " " + opti[1] + " " + answer
+                    helper /= 10
+                }
+                helper % 100 == 0 -> {
+                    count += 1
+                    helper /= 100
+                    answer = opti[2] + " " + answer
+                }
+                helper % 100 == 10 -> {
+                    answer = deg12[10] + " " + opti[2] + " " + answer
+                    helper /= 100
+                    count += 1
+                }
+                else -> {
+                    answer = deg12[helper % 10] + " " + opti[2] + " " + answer
+                    helper /= 10
+                }
+            }
+            continue
+        }
+        if (helper % 10 == 0) {
+            helper /= 10
+            continue
+        }
+        if (count == 1) answer = deg12[helper % 10] + answer
+        if ((count == 2) or (count == 5)) answer = deg2[helper % 10 - 2] + " " + answer
+        if ((count == 3) or (count == 6)) answer = deg3[helper % 10 - 1] + " " + answer
+        helper /= 10
+    }
+    return answer.trim()
+}
